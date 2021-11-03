e44fbd375965028aab3edd6dcae3ab4da5475922
Rob Nugen <rob@robnugen.com>
2021-11-03 10:31:09 +0900

    Add PDO database to container
    and hide deets from git via
    
    git update-index --assume-unchanged config/settings.php

--- config/container.php
+++ config/container.php
@@ -35,4 +35,18 @@ return [
         );
     },
 
+    PDO::class => function (ContainerInterface $container) {
+        $settings = $container->get('settings')['db'];
+
+        $host = $settings['host'];
+        $dbname = $settings['database'];
+        $username = $settings['username'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+        $flags = $settings['flags'];
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+        return new PDO($dsn, $username, $password, $flags);
+    },
+
 ];
