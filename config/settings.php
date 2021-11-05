7da270f849c5f2639d1ed8adf1d86e2545289182
Rob Nugen <rob@robnugen.com>
2021-11-05 17:58:36 +0900

    Add the rest of the database settings

---
+++ config/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+// Load default settings
+$settings = require __DIR__ . '/defaults.php';
+
+// Overwrite default settings with environment specific local settings
+if (file_exists(__DIR__ . '/../../poppopchaos.private_settings.php')) {
+    require __DIR__ . '/../../poppopchaos.private_settings.php';
+}
+
+return $settings;
