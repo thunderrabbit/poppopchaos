389bace8e9bfbdf3e3eb8048214448b6cf3d58a6
Rob Nugen <rob@robnugen.com>
2021-11-05 17:57:14 +0900

    Add most of the database settings

--- config/settings_default.php
+++ config/settings_default.php
@@ -31,4 +31,31 @@ $settings['error'] = [
     'log_error_details' => true,
 ];
 
+// Database settings
+$settings['db'] = [
+    'driver' => 'mysql',
+/*
+    Explain what's missing and where it can be found.  (not in repo)   See settings.php
+    'host' => 'OVERWRITE IN ../../poppopchaos.private_settings.php',
+    'database' => 'OVERWRITE IN ../../poppopchaos.private_settings.php',
+    'username' => 'OVERWRITE IN ../../poppopchaos.private_settings.php',
+    'password' => 'OVERWRITE IN ../../poppopchaos.private_settings.php',
+*/
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'flags' => [
+        // Turn off persistent connections
+        PDO::ATTR_PERSISTENT => false,
+        // Enable exceptions
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // Set character set
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
+    ],
+];
+
+
 return $settings;
